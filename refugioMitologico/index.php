108821c77895c06d678295def7507402cb27353e
Pablo <pagira@alumnatflorida.es>
2026-01-16 09:06:42 +0100

    añadiendo index

---
+++ refugioMitologico/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Refugio de Criaturas Mitológicas</title>
+</head>
+<body>
+
+    <h1>Refugio de Criaturas Mitológicas</h1>
+
+    <p>Bienvenido al sistema de gestión del santuario.</p>
+
+    <ul>
+        <li><a href="views/crear.php">Crear criatura</a></li>
+        <li><a href="views/listar.php">Listar criaturas</a></li>
+    </ul>
+
+</body>
+</html>
