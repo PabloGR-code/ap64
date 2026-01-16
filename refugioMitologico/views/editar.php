e6b7e26a8c2ab0abbc598071d080a5dc08c7822d
Pablo <pagira@alumnatflorida.es>
2026-01-16 09:34:49 +0100

    añadiedo comandos

--- refugioMitologico/views/editar.php
+++ refugioMitologico/views/editar.php
@@ -1,33 +1,53 @@
 <?php
+require_once '../models/criaturaVoladora.php';
+require_once '../models/criaturaMarina.php';
 require_once '../controllers/criaturaController.php';
 
 $controller = new CriaturaController();
 $criaturas = $controller->listar();
 
+/* Comprobación de seguridad */
+if (!isset($_GET['id']) || !isset($criaturas[$_GET['id']])) {
+    header("Location: listar.php");
+    exit;
+}
+
 $id = $_GET['id'];
 $criatura = $criaturas[$id];
 ?>
 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Editar criatura</title>
+</head>
+<body>
+
 <h2>Editar criatura</h2>
+
+<p>
+    <strong>
+        <?= implode(" - ", $criatura->getDatos()) ?>
+    </strong>
+</p>
 
 <form action="../models/gestor.php" method="post">
     <input type="hidden" name="id" value="<?= $id ?>">
 
-    <p>
-        <strong>
-            <?= implode(" - ", $criatura->getDatos()) ?>
-        </strong>
-    </p>
-
     Salud:
-    <input type="text" name="salud" value=""><br><br>
+    <input type="text" name="salud"><br><br>
 
     Peligrosidad:
-    <input type="number" name="peligrosidad" value=""><br><br>
+    <input type="number" name="peligrosidad"><br><br>
 
-    <button name="editar">Guardar cambios</button>
+    <button type="submit" name="editar">Guardar cambios</button>
 </form>
 
 <br>
-<a href="listar.php">Volver al listado</a>
+<a href="listar.php">Volver</a>
+
+</body>
+</html>
+
 
