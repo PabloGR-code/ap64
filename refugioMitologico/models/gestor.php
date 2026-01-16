4fab398a558e8f63f91af2db65463385742edba9
Pablo <pagira@alumnatflorida.es>
2026-01-16 09:00:59 +0100

    añadiendo gestor.php

--- refugioMitologico/models/gestor.php
+++ refugioMitologico/models/gestor.php
@@ -1,10 +1,38 @@
 <?php
+require_once '../controllers/CriaturaController.php';
+require_once '../models/Voladora.php';
+require_once '../models/Marina.php';
 
-    class gestorCriatura {
+$controller = new CriaturaController();
 
-        public function __construct(){
-            if(!isset($_SESSION['criatura'])){
-                $_SESSION['criatura']=[];
-            }
-        }
+if (isset($_POST['crear'])) {
+    if ($_POST['tipo'] == 'voladora') {
+        $criatura = new Voladora(
+            $_POST['nombre'],
+            $_POST['especie'],
+            $_POST['peligrosidad'],
+            $_POST['salud'],
+            $_POST['extra']
+        );
+    } else {
+        $criatura = new Marina(
+            $_POST['nombre'],
+            $_POST['especie'],
+            $_POST['peligrosidad'],
+            $_POST['salud'],
+            $_POST['extra']
+        );
     }
+    $controller->crear($criatura);
+    header("Location: ../views/listar.php");
+}
+
+if (isset($_POST['editar'])) {
+    $controller->editar($_POST['id'], $_POST['salud'], $_POST['peligrosidad']);
+    header("Location: ../views/listar.php");
+}
+
+if (isset($_POST['eliminar'])) {
+    $controller->eliminar($_POST['id']);
+    header("Location: ../views/listar.php");
+}
