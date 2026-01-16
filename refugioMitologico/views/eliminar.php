b64c6be39ded600be406bf7b72738db12685b6bf
Pablo <pagira@alumnatflorida.es>
2026-01-16 09:30:18 +0100

    añadiendo cambios

--- refugioMitologico/views/eliminar.php
+++ refugioMitologico/views/eliminar.php
@@ -6,19 +6,13 @@ require_once '../controllers/criaturaController.php';
 $controller = new CriaturaController();
 $criaturas = $controller->listar();
 
+/* COMPROBACIÓN CLAVE */
+if (!isset($_GET['id']) || !isset($criaturas[$_GET['id']])) {
+    header("Location: listar.php");
+    exit;
+}
+
 $id = $_GET['id'];
 $criatura = $criaturas[$id];
 ?>
 
-<h2>Eliminar criatura</h2>
-
-<p>¿Seguro que quieres eliminar esta criatura?</p>
-
-<p><strong><?= implode(" - ", $criatura->getDatos()) ?></strong></p>
-
-<form action="../models/gestor.php" method="post">
-    <input type="hidden" name="id" value="<?= $id ?>">
-    <button type="submit" name="eliminar">Sí, eliminar</button>
-</form>
-
-<a href="listar.php">Cancelar</a>
