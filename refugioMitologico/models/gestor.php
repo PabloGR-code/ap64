20d4125e75e4170c2a352be3d19c1e667eeceee6
Pablo <pagira@alumnatflorida.es>
2026-01-23 08:58:07 +0100

    añadiendo gestor

--- refugioMitologico/models/gestor.php
+++ refugioMitologico/models/gestor.php
@@ -1,38 +1,30 @@
 <?php
-require_once '../models/criaturaVoladora.php';
-require_once '../models/criaturaMarina.php';
-require_once '../controllers/criaturaController.php';
+require_once '../models/CRUDinterface.php';
 
-$controller = new CriaturaController();
+class Gestor implements CRUDinterface{
+    private $datos=[];
 
-if (isset($_POST['crear'])) {
-    if ($_POST['tipo'] == 'voladora') {
-        $criatura = new Voladora(
-            $_POST['nombre'],
-            $_POST['especie'],
-            $_POST['peligrosidad'],
-            $_POST['salud'],
-            $_POST['extra']
-        );
-    } else {
-        $criatura = new Marina(
-            $_POST['nombre'],
-            $_POST['especie'],
-            $_POST['peligrosidad'],
-            $_POST['salud'],
-            $_POST['extra']
-        );
+    public function agregar($objeto){
+        $this->datos[]=$objeto;
     }
-    $controller->crear($criatura);
-    header("Location: ../views/listar.php");
-}
 
-if (isset($_POST['editar'])) {
-    $controller->editar($_POST['id'], $_POST['salud'], $_POST['peligrosidad']);
-    header("Location: ../views/listar.php");
-}
+    public function listar(){
+        return $this->datos;
+    }
 
-if (isset($_POST['eliminar'])) {
-    $controller->eliminar($_POST['id']);
-    header("Location: ../views/listar.php");
+    public function obtenerPorId($id){
+        return $this->datos[$id] ?? null;
+    }
+
+    public function actualizar($id, $objeto){
+        if(isset($this->datos[$id])) {
+            $this->datos[$id]=$objeto;     
+        }
+    }
+
+    public function eliminar($id){
+        id(isset($this->datos[$id])){
+            unset($this->datos[$id]);
+        }
+    }
 }
